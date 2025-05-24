a61aa92e8d1e39029ff96fa4eced4e746f194d66
Gibran <asshadiqi.gibran@gmail.com>
2025-05-24 05:56:35 +0000

    hotfix supervisor for fetch and gather. Still need manual supervisor

--- ckan/docker-entrypoint.d/02_setup_harvest.sh
+++ ckan/docker-entrypoint.d/02_setup_harvest.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
-
 if [[ $CKAN__PLUGINS == *"harvest ckan_harvester"* ]]; then
-   echo "Set up ckan.harvest"
-   ckan config-tool $CKAN_INI db upgrade -p harvest || echo "ckanext-harvest initdb command finished (may have already run or encountered a non-critical issue)."
-   service apache2 restart
-   supervisorctl reread
-   supervisorctl add ckan_gather_consumer
-   supervisorctl add ckan_fetch_consumer
-   supervisorctl start ckan_gather_consumer
-   supervisorctl start ckan_fetch_consumer
-   supervisorctl enable ckan_gather_consumer
-   supervisorctl enable ckan_fetch_consumer
-   echo "*/30 * * * * ckan /usr/local/bin/ckan_harvester_job.sh >> /var/log/ckan/harvester_cron.log 2>&1" > /etc/cron.d/ckan_harvester_cron
-   chmod 0644 /etc/cron.d/ckan_harvester_cron
+   echo "--- Starting CKAN HARVEST configuration ---"
+   #echo "CKAN_INI: $CKAN_INI"
+
+   ckan -c $CKAN_INI db upgrade -p harvest || {
+   echo "ERROR: Failed to do a DB Upgrade for CKAN HARVEST"
+   exit 1
+   }
+
 else
-   echo "Not configuring Harvester"
+   echo "INFO: Not configuring Harvester"
 fi
